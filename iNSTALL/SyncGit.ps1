1767fe36d3247e657edd5473c3cb49417762e24a
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2018-09-04 23:23:41 +0200

    template

--- iNSTALL/SyncGit.ps1
+++ iNSTALL/SyncGit.ps1
@@ -4,12 +4,15 @@
 # Change SecurityProtocol for downloading from GitHub
 $SavedSecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol
 [System.Net.ServicePointManager]::SecurityProtocol = 'Tls,Tls11,Tls12'
+# Download Archive
 (New-Object System.Net.WebClient).downloadFile($UrlDownload,$Output)
 [System.Net.ServicePointManager]::SecurityProtocol = $SavedSecurityProtocol
 # Unzip Archive to Temp
 Expand-Archive -Path $Output -DestinationPath "$ENV:TEMP\Template" -Force
 # Cleanup and Copy iNSTALL Folder
-get-childitem -path "c:\install" -Recurse  | Remove-Item -Recurse -Force -Verbose
-Copy-Item -Path "$ENV:TEMP\Template\BizzCloudWinTemplates-master\iNSTALL\*" -Destination 'c:\iNSTALL' -Recurse -Force -Verbose
+get-childitem -path "c:\install" -Recurse  | Remove-Item -Recurse -Force
+Copy-Item -Path "$ENV:TEMP\Template\BizzCloudWinTemplates-master\iNSTALL\*" -Destination 'c:\iNSTALL' -Recurse -Force
 # Cleanup Temp Folder
-get-childitem -path "$ENV:TEMP\Template" -Recurse  | Remove-Item -Force -Recurse -Verbose
+remove-Item -Path "$Output" -Force
+remove-item -path "$ENV:TEMP\Template" -Recurse -Force
+# The End
