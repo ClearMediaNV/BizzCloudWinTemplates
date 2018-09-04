13372aeec9ea7d967b69ff213d2af84d931cbdb3
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2018-09-04 23:27:51 +0200

    template

--- iNSTALL/SyncGit.ps1
+++ iNSTALL/SyncGit.ps1
@@ -10,17 +10,9 @@ $SavedSecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol
 # Unzip Archive to Temp
 Expand-Archive -Path $Output -DestinationPath "$ENV:TEMP\Template" -Force
 # Cleanup and Copy iNSTALL Folder
-<<<<<<< HEAD
 get-childitem -path "c:\install" -Recurse  | Remove-Item -Recurse -Force
 Copy-Item -Path "$ENV:TEMP\Template\BizzCloudWinTemplates-master\iNSTALL\*" -Destination 'c:\iNSTALL' -Recurse -Force
 # Cleanup Temp Folder
 remove-Item -Path "$Output" -Force
 remove-item -path "$ENV:TEMP\Template" -Recurse -Force
-=======
-Remove-Item -Path "c:\install\*" -Recurse -Force
-Copy-Item -Path "$ENV:TEMP\Template\BizzCloudWinTemplates-master\iNSTALL\*" -Destination 'c:\iNSTALL' -Recurse -Force
-# Cleanup Temp Folder
-Remove-Item -Path "$Output" -Force
-Remove-Item -Path "$ENV:TEMP\Template" -Recurse -Force
->>>>>>> 504e490bda7a224ed714c738b4554159e7da255a
 # The End
