bacb21e9c984c1e32aad74cf3b740608afe39493
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2018-09-08 15:06:12 +0200

    template

--- iNSTALL/Deployment/AioSetup/InstallRDS.ps1
+++ iNSTALL/Deployment/AioSetup/InstallRDS.ps1
@@ -1,1 +1,2 @@
+# Installation of RDS Session Host will restart the System
 Install-windowsfeature RDS-RD-Server, RDS-Licensing, RDS-Licensing-UI, RSAT-RDS-Licensing-Diagnosis-UI, RSAT-Print-Services, RSAT-File-Services, RSAT-FSRM-Mgmt, Server-Media-Foundation, Windows-TIFF-IFilter -restart
