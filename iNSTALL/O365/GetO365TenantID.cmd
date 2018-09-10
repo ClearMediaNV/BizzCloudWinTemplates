f69c614ccd5462ed74311c1d0fa5d05ba54ded19
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2018-09-10 21:51:35 +0200

    Update and rename Get_O365_TenantID.cmd to GetO365TenantID.cmd

---
+++ iNSTALL/O365/GetO365TenantID.cmd
@@ -0,0 +1,2 @@
+Pushd %0\..\
+powershell.exe -Noninteractive -WindowStyle Hidden -File GetO365TenantID.ps1
--- iNSTALL/O365/Get_O365_TenantID.cmd
+++
@@ -1,2 +0,0 @@
-Pushd %0\..\
-powershell.exe -Noninteractive -WindowStyle Hidden -File Get_O365_TenantID.ps1
