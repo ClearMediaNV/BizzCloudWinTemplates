b98f5176ceb02b3116e8e02af288dc4c7757c3eb
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2018-11-19 16:41:49 +0100

    add GPMC

--- iNSTALL/Deployment/AioSetup/InstallRDS.ps1
+++ iNSTALL/Deployment/AioSetup/InstallRDS.ps1
@@ -4,5 +4,6 @@ Set-Service -Name 'Audiosrv' -StartupType 'Auto'
 Install-WindowsFeature -Name Windows-TIFF-IFilter
 Install-WindowsFeature -Name RDS-RD-Server
 Install-WindowsFeature -Name RDS-Licensing
+Install-WindowsFeature -Name GPMC
 Install-WindowsFeature -Name RDS-Licensing-UI, RSAT-RDS-Licensing-Diagnosis-UI, RSAT-Print-Services, RSAT-File-Services, RSAT-FSRM-Mgmt, Server-Media-Foundation
 Restart-Computer
