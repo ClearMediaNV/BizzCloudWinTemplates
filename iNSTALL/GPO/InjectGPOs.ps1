21b68fd6e59194139e50aaf7d7087049335e122a
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2018-12-20 15:58:03 +0100

    GPO
    O365 ADM Templates
    Standard GPOs

---
+++ iNSTALL/GPO/InjectGPOs.ps1
@@ -0,0 +1,14 @@
+﻿Push-Location -Path 'c:\install\gpo'
+$GpoNameList = ('RDS Server Policy','Standard User Policy','Standard O365 User Policy')
+$GpoNameList | Where-Object {
+    $GpoName = $_
+    new-gpo -Name "$GpoName" -ErrorAction Ignore
+    $c = Import-Csv -Path "$GpoName.csv"
+    $c | ForEach-Object { 
+                    if ( $_.Type -eq 'Dword' ) { [INT]$Value = $_.Value } else { [STRING]$Value = $_.Value }
+                    Set-GPRegistryValue -Name "$GpoName" -Key $_.Key -Type $_.Type -ValueName $_.ValueName -Value $Value }
+
+    }
+
+
+
