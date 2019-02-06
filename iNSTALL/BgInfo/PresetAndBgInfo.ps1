4ee52040c53764f9a0e9601df9b110a9eeaaa389
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2019-02-06 13:19:19 +0100

    Update PresetAndBgInfo.ps1 TCPIP6 - NLA

--- iNSTALL/BgInfo/PresetAndBgInfo.ps1
+++ iNSTALL/BgInfo/PresetAndBgInfo.ps1
@@ -7,6 +7,10 @@ Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\Disk' -Name 'Tim
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\ServerManager' -Name 'DoNotOpenServerManagerAtLogon' -Value 1
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Internet Explorer\Main\' -Name 'start page' -Value 'about:blank'
 Get-LocalUser | Where-Object { $_.SID -like '*-500'} | Set-LocalUser -PasswordNeverExpires $TRUE
+#Tcpip6-NLA
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters' -Name 'TimeOutValue' -Value 0
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\NlaSvc\Parameters\Internet' -Name 'EnableActiveProbing' -Value 1
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\NlaSvc\Parameters\Internet' -Name 'WebTimeout' -Value 23
 # Get Public IP @
 Set-Item -Path 'ENV:\IpAddressPublic' -Value '0.0.0.0'
 Set-Item -Path 'ENV:\IpAddressPublic' -Value (Invoke-WebRequest -Uri 'https://api.ipify.org' -UseBasicParsing).content
