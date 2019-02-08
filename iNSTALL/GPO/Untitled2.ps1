00e5a462d08848d2445dbd8afa0e59f03a74fa7b
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2019-02-08 12:59:21 +0100

    update

--- iNSTALL/GPO/Untitled2.ps1
+++
@@ -1,2 +0,0 @@
-﻿$json = Invoke-WebRequest -Uri https://api.github.com/repos/guyjochmans/cmscripts -Method Get -ErrorAction Stop
-Invoke-WebRequest -Body $RequestBody -Uri https://api.github.com/repos/guyjochmans/cmscripts/pulls -Method Get -Headers @{"Authorization"="Basic $BasicCreds"} -ErrorAction Stop
