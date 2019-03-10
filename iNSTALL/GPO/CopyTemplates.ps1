1d8bae744fa50ab171bbcab251359395c669c17a
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2019-03-10 23:04:26 +0100

    Delete CopyTemplates.ps1

--- iNSTALL/GPO/CopyTemplates.ps1
+++
@@ -1,9 +0,0 @@
-﻿Copy-Item -Path 'C:\iNSTALL\GPO\Templates\admx\*' -Destination 'C:\Windows\PolicyDefinitions' -Force
-Copy-Item -Path 'C:\iNSTALL\GPO\Templates\admx\en-US\*' -Destination 'C:\Windows\PolicyDefinitions\en-US' -Force
-Copy-Item -Path 'C:\iNSTALL\GPO\Templates\admx\fr-FR\*' -Destination 'C:\Windows\PolicyDefinitions\fr-FR' -Force
-New-Item -Path 'C:\Windows\SYSVOL\domain\Policies\PolicyDefinitions' -ItemType Directory -Force
-Copy-Item -Path 'C:\Windows\PolicyDefinitions\*' -Destination 'C:\Windows\SYSVOL\domain\Policies\PolicyDefinitions' -Force
-New-Item -Path 'C:\Windows\SYSVOL\domain\Policies\PolicyDefinitions\en-US' -ItemType Directory -Force
-New-Item -Path 'C:\Windows\SYSVOL\domain\Policies\PolicyDefinitions\fr-FR' -ItemType Directory -Force
-Copy-Item -Path 'C:\Windows\PolicyDefinitions\en-US\*' -Destination 'C:\Windows\SYSVOL\domain\Policies\PolicyDefinitions\en-US' -Force
-Copy-Item -Path 'C:\Windows\PolicyDefinitions\fr-FR\*' -Destination 'C:\Windows\SYSVOL\domain\Policies\PolicyDefinitions\fr-FR' -Force
