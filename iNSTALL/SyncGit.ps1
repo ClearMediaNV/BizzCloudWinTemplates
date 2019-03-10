a79b1b9120bf356ec46b17cfd02873decdc07f3f
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2019-03-11 00:38:38 +0100

    Update SyncGit.ps1

--- iNSTALL/SyncGit.ps1
+++ iNSTALL/SyncGit.ps1
@@ -1,6 +1,6 @@
 # Get GitHub Repository Archive
 [STRING]$UrlDownload =  'https://github.com/ClearMediaNV/BizzCloudWinTemplates/archive/master.zip'
-[STRING]$Output = "$($ENV:TEMP)\$($UrlDownload.Split('/')[$_.count-1])"
+[STRING]$Output = "$ENV:TEMP\$($UrlDownload.Split('/')[$_.count-1])"
 # Change SecurityProtocol for downloading from GitHub
 $SavedSecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol
 [System.Net.ServicePointManager]::SecurityProtocol = 'Tls,Tls11,Tls12'
@@ -11,8 +11,8 @@ $SavedSecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol
 Expand-Archive -Path $Output -DestinationPath "$ENV:TEMP\Template" -Force
 # Cleanup and Copy iNSTALL Folder
 Remove-Item -Path 'c:\install\*'  -Recurse -Force
-Copy-Item -Path "$($ENV:TEMP)\Template\BizzCloudWinTemplates-master\iNSTALL\*" -Destination 'c:\iNSTALL' -Recurse -Force
+Copy-Item -Path "$ENV:TEMP\Template\BizzCloudWinTemplates-master\iNSTALL\*" -Destination 'c:\iNSTALL' -Recurse -Force
 # Cleanup Temp Folder
 Remove-Item -Path "$Output" -Force
-Remove-Item -Path "$($ENV:TEMP)\Template" -Recurse -Force
+Remove-Item -Path "$ENV:TEMP\Template" -Recurse -Force
 # The End
