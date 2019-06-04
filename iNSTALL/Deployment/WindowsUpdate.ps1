48fd848e3727efc47ea456646dcfeb62ff52c9a4
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2019-06-05 01:13:52 +0200

    Update WindowsUpdate.ps1

--- iNSTALL/Deployment/WindowsUpdate.ps1
+++ iNSTALL/Deployment/WindowsUpdate.ps1
@@ -14,7 +14,7 @@ $WindowsUpdateInstall = New-Object -ComObject 'Microsoft.Update.Installer'
 Try	{ $WindowsUpdateList = $WindowsUpdateSearch.Search($Null).Updates }
 	Catch 
 		{
-        	Write-Output 'WSUS not Reachable. No Internet Connection. Please Check DNS & Gateway Config'
+        	Write-Output 'WSUS not Reachable. No Internet Connection. Please Check DNS & Gateway Config.'
         	Start-Sleep -Seconds 5
 		Exit
 		}
