2133a2e82d51f825c2e97e8990b50bf5344bbc0b
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2019-12-14 14:39:23 +0100

    Update PatchOstPath.ps1

--- iNSTALL/Deployment/PatchOstPath.ps1
+++ iNSTALL/Deployment/PatchOstPath.ps1
@@ -25,8 +25,7 @@ If ( (Get-ChildItem -Path 'HKCU:\SOFTWARE\Microsoft\Office\16.0\Outlook\Profiles
     Start-Sleep -Seconds 5
     Exit
     }
-[BYTE[]]$PathArray = $Null
-$OstPath.ToCharArray() | ForEach-Object { $PathArray += ($_,0) } ; $PathArray += (0,0)
+[BYTE[]]$PathArray = $Null ; $OstPath.ToCharArray() | ForEach-Object { $PathArray += ($_,0) } ; $PathArray += (0,0)
 If  ( Get-ChildItem -Path "HKCU:\SOFTWARE\Microsoft\Office\16.0\Outlook\Profiles\$MapiProfile" | Where-Object { $_.Property -eq '001f6610' } )
     {
     Get-ChildItem -Path "HKCU:\SOFTWARE\Microsoft\Office\16.0\Outlook\Profiles\$MapiProfile" | Where-Object { $_.Property -eq '001f6610' } | ForEach-Object { Set-ItemProperty -LiteralPath $_.PSPath   -Name '001f6610' -Value $PathArray }
