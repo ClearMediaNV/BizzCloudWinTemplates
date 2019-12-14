5a6c72912560fb39f05470636513cdee7c106370
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2019-12-14 14:22:42 +0100

    Update DeleteTeams.ps1

--- iNSTALL/Deployment/DeleteTeams.ps1
+++ iNSTALL/Deployment/DeleteTeams.ps1
@@ -1,13 +1,13 @@
 $TeamsPath = "$Env:LOCALAPPDATA\Microsoft\Teams"
 $TeamsUpdateExePath = "$($TeamsPath)\Update.exe"
 try {
-    if (Test-Path -Path $TeamsUpdateExePath) {
+    if ( Test-Path -Path $TeamsUpdateExePath ) {
         Write-Output ' Uninstalling Teams process'
         # Uninstall app
         $proc = Start-Process -FilePath $TeamsUpdateExePath -ArgumentList '-uninstall -s' -PassThru
         $proc.WaitForExit()
         }
-    if (Test-Path -Path $TeamsPath) {
+    if ( Test-Path -Path $TeamsPath ) {
         Write-Output ' Deleting Teams directory'
         Remove-Item -Path $TeamsPath -Recurse -Force 
         }
