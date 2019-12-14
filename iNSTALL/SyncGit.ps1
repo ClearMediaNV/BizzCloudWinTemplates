a71e32e00f5c7e21c2496dd4544ed2ae9e8a88e1
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2019-12-14 13:37:01 +0100

    Update SyncGit.ps1

--- iNSTALL/SyncGit.ps1
+++ iNSTALL/SyncGit.ps1
@@ -8,7 +8,7 @@ Try {
     $FileDownload = "$ENV:TEMP\$Branch.zip"
     $FolderDownload = "$ENV:TEMP\$Branch"
     # Download Archive
-    (New-Object System.Net.WebClient).downloadFile($UrlDownload,$FileDownload)
+    [System.Net.WebClient]::new().downloadFile($UrlDownload,$FileDownload)
     # Unzip Archive to Temp Folder
     [VOID][System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem")
     [System.IO.Compression.ZipFile]::ExtractToDirectory($FileDownload, "$FolderDownload") 
