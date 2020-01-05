2022311ccef1f06f886a0836aee670a78f322bcd
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2020-01-05 15:02:01 +0100

    Update DisableTsFairShare.ps1

--- iNSTALL/Deployment/DisableTsFairShare.ps1
+++ iNSTALL/Deployment/DisableTsFairShare.ps1
@@ -1,10 +1,1 @@
 Get-CimInstance -Namespace 'root/cimv2/TerminalServices' -ClassName Win32_TerminalServiceSetting | Set-CimInstance -argument  @{EnableDFSS=1;EnableDiskFSS=1;EnableNetworkFSS=1}
-
-Windows Registry Editor Version 5.00
-
-[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Quota System]
-"EnableCpuQuota"=dword:0
-[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\TSFairShare\Disk]
-"EnableFairShare"=dword:0
-[HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\TSFairShare\NetFS]
-"EnableFairShare"=dword:0
