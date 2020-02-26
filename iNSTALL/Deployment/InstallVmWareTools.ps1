1aef2f21adeb7fc839ac31056df29856207d44e7
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2020-02-27 00:15:28 +0100

    Create InstallVmWareTools.ps1

---
+++ iNSTALL/Deployment/InstallVmWareTools.ps1
@@ -0,0 +1,7 @@
+# Download & Install Chocolatey as PackageProvider
+[System.Net.ServicePointManager]::SecurityProtocol = 'Tls,Tls11,Tls12'
+Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')
+# Install vmware-tools version 11.0.5.15389592
+Invoke-Expression -Command '& C:\ProgramData\chocolatey\choco install vmware-tools --version 11.0.5.15389592 -y'
+$ExitCode = $LASTEXITCODE
+If ( $ExitCode -eq 3010 ) { Restart-Computer }
