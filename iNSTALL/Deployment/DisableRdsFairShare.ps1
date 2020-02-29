27a660a2e6cf7f6d487c874519124a8b9f5c026a
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2020-02-29 21:09:53 +0100

    Update DisableRdsFairShare.ps1

--- iNSTALL/Deployment/DisableRdsFairShare.ps1
+++ iNSTALL/Deployment/DisableRdsFairShare.ps1
@@ -1,1 +1,1 @@
-Get-CimInstance -Namespace 'root/cimv2/TerminalServices' -ClassName Win32_TerminalServiceSetting | Set-CimInstance -argument  @{EnableDFSS=0;EnableDiskFSS=0;EnableNetworkFSS=0}
+Get-CimInstance -Namespace 'root/cimv2/TerminalServices' -ClassName 'Win32_TerminalServiceSetting' | Set-CimInstance -argument  @{EnableDFSS=0;EnableDiskFSS=0;EnableNetworkFSS=0}
