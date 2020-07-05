845b37d331a330c550761522818e527e2c2fc8c4
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2020-07-05 22:27:20 +0200

    Update PresetAndBgInfo.ps1

--- iNSTALL/BgInfo/PresetAndBgInfo.ps1
+++ iNSTALL/BgInfo/PresetAndBgInfo.ps1
@@ -18,7 +18,7 @@ Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\NlaSvc\Parameter
 Set-Item -Path 'ENV:\IpAddressPublic' -Value '0.0.0.0'
 Set-Item -Path 'ENV:\IpAddressPublic' -Value (Invoke-WebRequest -Uri 'https://api.ipify.org' -UseBasicParsing).content
 # Restart NLA Service for DomainController
-Get-CimInstance -NameSpace root/CIMV2 -ClassName win32_ComputerSystem | ForEach-Object { if ( $PSItem.DomainRole -in ( 4 ,5 ) ) { Restart-Service -Name 'NlaSvc' -Force } }
+Get-CimInstance -NameSpace root/CIMV2 -ClassName win32_ComputerSystem | ForEach-Object { if ( $PSItem.DomainRole -in ( 4 , 5 ) ) { Restart-Service -Name 'NlaSvc' -Force } }
 # Get NLA state
 Set-Item -Path 'ENV:\NetworkCategory' -value (Get-NetConnectionProfile).NetworkCategory
 Set-Item -Path 'ENV:\IPv4Connectivity' -Value (Get-NetConnectionProfile).IPv4Connectivity
