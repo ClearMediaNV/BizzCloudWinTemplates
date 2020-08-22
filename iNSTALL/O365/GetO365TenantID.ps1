41275a18f34777d34ea1997939ccad7a0f73e8b1
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2020-08-22 22:55:42 +0200

    Update GetO365TenantID.ps1

--- iNSTALL/O365/GetO365TenantID.ps1
+++ iNSTALL/O365/GetO365TenantID.ps1
@@ -2,7 +2,7 @@ Add-Type -AssemblyName System.Windows.Forms
 
 # Create Form
 $Form = New-Object System.Windows.Forms.Form 
-$Form.Text = 'Get O365 TenantID'
+$Form.Text = ' Get O365 TenantID'
 $Form.showIcon = $false
 $Form.MinimizeBox = $false
 $Form.MaximizeBox = $false
@@ -15,7 +15,7 @@ $Form.Topmost = $False
 # Add Action ScriptBlocks
 $ScriptBlockOK = {
     try {
-        [STRING]$Domainname = $TextBoxDomain.Text
+        [STRING]$Domainname = $TextBoxDomainName.Text
         $TextBoxTenantID.Text = (Invoke-RestMethod -Uri "https://login.windows.net/$Domainname/.well-known/openid-configuration").token_endpoint.Split('/')[3]
         $TextBoxTenantID.BackColor = 'LightGreen'
         $TextBoxTenantID.Refresh()
@@ -30,70 +30,92 @@ $ScriptBlockCspDelegation = {
     $Url = 'https://portal.office.com/partner/partnersignup.aspx?type=ResellerRelationship&id=f59f16d4-ed58-42e1-92c1-af863f919035&csp=1&msppid=0'
     Start-Process $Url
     }
-$CopyToClipboardDomain = { Set-Clipboard -Value $TextBoxDomain.Text }
-$CopyToClipboardTenantID = { Set-Clipboard -Value $TextBoxTenantID.Text }
+$ScriptBlockCopyToClipboardDomain = { Set-Clipboard -Value $TextBoxDomainName.Text }
+$ScriptBlockCopyToClipboardTenantID = { Set-Clipboard -Value $TextBoxTenantID.Text }
 
 # Add OK Button
 # On Click or Enter launch ScriptBlock
-$OKButton = New-Object System.Windows.Forms.Button
-$OKButton.Left = 40
-$OKButton.Top = 150
-$OKButton.Width = 75
-$OKButton.Height = 40
-$OKButton.BackColor = 'White'
-$OKButton.Text = 'OK'
-$OKButton.Add_Click($ScriptBlockOK)
-$Form.AcceptButton = $OKButton
-$Form.Controls.Add($OKButton)
+$ButtonOK = New-Object System.Windows.Forms.Button
+$ButtonOK.Left = 40
+$ButtonOK.Top = 150
+$ButtonOK.Width = 75
+$ButtonOK.Height = 40
+$ButtonOK.BackColor = 'White'
+$ButtonOK.Text = 'OK'
+$ButtonOK.Add_Click($ScriptBlockOK)
+$Form.AcceptButton = $ButtonOK
+$Form.Controls.Add($ButtonOK)
 
 # Add CspDelegation Button
-$CspDelegationButton = New-Object System.Windows.Forms.Button
-$CspDelegationButton.Left = 120
-$CspDelegationButton.Top = 150
-$CspDelegationButton.Width = 290
-$CspDelegationButton.Height = 40
-$CspDelegationButton.BackColor = 'LightYellow'
-$CspDelegationButton.ForeColor = 'Red'
-$CspDelegationButton.Font = new-object System.Drawing.Font('',8,[System.Drawing.FontStyle]::Bold)
-$CspDelegationButton.Text = 'Start CSP Delegation to ClearMedia NV'
-$CspDelegationButton.Add_Click($ScriptBlockCspDelegation)
-$Form.Controls.Add($CspDelegationButton)
+$ButtonCspDelegation = New-Object System.Windows.Forms.Button
+$ButtonCspDelegation.Left = 120
+$ButtonCspDelegation.Top = 150
+$ButtonCspDelegation.Width = 290
+$ButtonCspDelegation.Height = 40
+$ButtonCspDelegation.BackColor = 'LightYellow'
+$ButtonCspDelegation.ForeColor = 'Red'
+$ButtonCspDelegation.Font = new-object System.Drawing.Font('',8,[System.Drawing.FontStyle]::Bold)
+$ButtonCspDelegation.Text = 'Start CSP Delegation to ClearMedia NV'
+$ButtonCspDelegation.Add_Click($ScriptBlockCspDelegation)
+$Form.Controls.Add($ButtonCspDelegation)
 
 
 # Add Cancel Button
-$CancelButton = New-Object System.Windows.Forms.Button
-$CancelButton.Left = 415
-$CancelButton.Top = 150
-$CancelButton.Width = 75
-$CancelButton.Height = 40
-$CancelButton.BackColor = 'White'
-$CancelButton.Text = 'CANCEL'
-$Form.CancelButton = $CancelButton
-$Form.Controls.Add($CancelButton)
+$ButtonCancel = New-Object System.Windows.Forms.Button
+$ButtonCancel.Left = 415
+$ButtonCancel.Top = 150
+$ButtonCancel.Width = 75
+$ButtonCancel.Height = 40
+$ButtonCancel.BackColor = 'White'
+$ButtonCancel.Text = 'CANCEL'
+$Form.CancelButton = $ButtonCancel
+$Form.Controls.Add($ButtonCancel)
 
-# Add Label
-$LabelDomain = New-Object System.Windows.Forms.Label
-$LabelDomain.AutoSize = $false
-$LabelDomain.Top = 25
-$LabelDomain.Left = 160
-$LabelDomain.Width = 400
-$LabelDomain.Height = 30
-$LabelDomain.Font = 'Candara , 11pt, style=Regular'
-$LabelDomain.Text = 'Please enter the Domain Name'
-$Form.Controls.Add($LabelDomain)
+# Add CopyToClipboardDomain Button
+$ButtonCopyToClipboardDomain = New-Object System.Windows.Forms.Button
+$ButtonCopyToClipboardDomain.Left = 500
+$ButtonCopyToClipboardDomain.Top = 65
+$ButtonCopyToClipboardDomain.Width = 50
+$ButtonCopyToClipboardDomain.Height = 30
+$ButtonCopyToClipboardDomain.BackColor = 'White'
+$ButtonCopyToClipboardDomain.Text = 'Copy'
+$ButtonCopyToClipboardDomain.Add_Click($ScriptBlockCopyToClipboardDomain)
+$Form.Controls.Add($ButtonCopyToClipboardDomain)
+
+# Add CopyToClipboardTenantID Button
+$ButtonCopyToClipboardTenantID = New-Object System.Windows.Forms.Button
+$ButtonCopyToClipboardTenantID.Left = 500
+$ButtonCopyToClipboardTenantID.Top = 95
+$ButtonCopyToClipboardTenantID.Width = 50
+$ButtonCopyToClipboardTenantID.Height = 30
+$ButtonCopyToClipboardTenantID.BackColor = 'White'
+$ButtonCopyToClipboardTenantID.Text = 'Copy'
+$ButtonCopyToClipboardTenantID.Add_Click($ScriptBlockCopyToClipboardTenantID)
+$Form.Controls.Add($ButtonCopyToClipboardTenantID)
+
+# Add DomaiName Label
+$LabelDomainName = New-Object System.Windows.Forms.Label
+$LabelDomainName.AutoSize = $false
+$LabelDomainName.Top = 25
+$LabelDomainName.Left = 160
+$LabelDomainName.Width = 400
+$LabelDomainName.Height = 30
+$LabelDomainName.Font = 'Candara , 11pt, style=Regular'
+$LabelDomainName.Text = 'Please enter the Domain Name'
+$Form.Controls.Add($LabelDomainName)
 
 # Add DomainName TextBox
-$TextBoxDomain = New-Object System.Windows.Forms.TextBox
-$TextBoxDomain.AutoSize = $false
-$TextBoxDomain.Top = 65
-$TextBoxDomain.Left = 40
-$TextBoxDomain.Width = 450
-$TextBoxDomain.Height = 30
-$TextBoxDomain.BackColor = 'green'
-$TextBoxDomain.Font = 'Candara , 14pt, style=Bold'
-$TextBoxDomain.TextAlign = 'Center'
-$TextBoxDomain.Text = 'ClearMedia.be'
-$Form.Controls.Add($TextBoxDomain)
+$TextBoxDomainName = New-Object System.Windows.Forms.TextBox
+$TextBoxDomainName.AutoSize = $false
+$TextBoxDomainName.Top = 65
+$TextBoxDomainName.Left = 40
+$TextBoxDomainName.Width = 450
+$TextBoxDomainName.Height = 30
+$TextBoxDomainName.BackColor = 'green'
+$TextBoxDomainName.Font = 'Candara , 14pt, style=Bold'
+$TextBoxDomainName.TextAlign = 'Center'
+$TextBoxDomainName.Text = 'ClearMedia.be'
+$Form.Controls.Add($TextBoxDomainName)
 
 # Add TenantID TextBox
 $TextBoxTenantID = New-Object System.Windows.Forms.TextBox
@@ -107,28 +129,5 @@ $TextBoxTenantID.Font = 'Candara , 14pt, style=Bold'
 $TextBoxTenantID.TextAlign = 'Center'
 $TextBoxTenantID.Text = ''
 $Form.Controls.Add($TextBoxTenantID)
-
-#copy to clipboard domain
-$CopyDomainButton = New-Object System.Windows.Forms.Button
-$CopyDomainButton.Left = 500
-$CopyDomainButton.Top = 65
-$CopyDomainButton.Width = 50
-$CopyDomainButton.Height = 30
-$CopyDomainButton.BackColor = 'White'
-$CopyDomainButton.Text = 'Copy'
-$CopyDomainButton.Add_Click($CopyToClipboardDomain)
-$Form.Controls.Add($CopyDomainButton)
-
-
-#copy to clipboard tenantid
-$CopyTenantIDButton = New-Object System.Windows.Forms.Button
-$CopyTenantIDButton.Left = 500
-$CopyTenantIDButton.Top = 95
-$CopyTenantIDButton.Width = 50
-$CopyTenantIDButton.Height = 30
-$CopyTenantIDButton.BackColor = 'White'
-$CopyTenantIDButton.Text = 'Copy'
-$CopyTenantIDButton.Add_Click($CopyToClipboardTenantID)
-$Form.Controls.Add($CopyTenantIDButton)
 
 $Form.ShowDialog()
