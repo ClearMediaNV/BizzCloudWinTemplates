af3f3d2121f063a68175ef558bfa1e5f7c9d1424
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2020-09-07 12:26:41 +0200

    Delete AddHyperVManagementTools.ps1

--- iNSTALL/Deployment/AddHyperVManagementTools.ps1
+++
@@ -1,4 +0,0 @@
-# Install only the PowerShell module
-Install-WindowsFeature -Name Hyper-V-PowerShell
-# Install Hyper-V Manager and the PowerShell module
-Install-WindowsFeature -Name RSAT-Hyper-V-Tools
