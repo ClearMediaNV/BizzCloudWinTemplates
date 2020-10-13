14c6d93e370c4b4cff4c105a8660320bb35ff5e9
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2020-10-13 21:56:13 +0200

    Update InstallRDS.ps1

--- iNSTALL/Deployment/AioSetup/InstallRDS.ps1
+++ iNSTALL/Deployment/AioSetup/InstallRDS.ps1
@@ -1,10 +1,13 @@
 # Installation of RDS Session Host will restart the System
+# Set RDS related Servives to Automatic Start
 Set-Service -Name 'WSearch' -StartupType 'Auto'
 Set-Service -Name 'SCardSvr' -StartupType 'Auto'
 Set-Service -Name 'Audiosrv' -StartupType 'Auto'
+# Install Essential RDS Features & Roles
 Install-WindowsFeature -Name Windows-TIFF-IFilter
 Install-WindowsFeature -Name RDS-RD-Server
 Install-WindowsFeature -Name RDS-Licensing
 Install-WindowsFeature -Name RDS-Licensing-UI, RSAT-RDS-Licensing-Diagnosis-UI
+# Disable RDS Fair Share on Cpu,Disk,Network Aka Disable Handbrake
 Get-CimInstance -Namespace 'root/cimv2/TerminalServices' -ClassName 'Win32_TerminalServiceSetting' | Set-CimInstance -Argument  @{EnableDFSS=0;EnableDiskFSS=0;EnableNetworkFSS=0}
 Restart-Computer
