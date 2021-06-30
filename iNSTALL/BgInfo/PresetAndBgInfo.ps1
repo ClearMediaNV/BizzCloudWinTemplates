573c1c75532feaace3df044129b1f804094890b5
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2021-06-30 23:45:16 +0200

    Update PresetAndBgInfo.ps1
    Add WSUS Server
    WSUS Server will be added to BgInfo Inventory

--- iNSTALL/BgInfo/PresetAndBgInfo.ps1
+++ iNSTALL/BgInfo/PresetAndBgInfo.ps1
@@ -14,8 +14,8 @@ Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameter
 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\NlaSvc\Parameters\Internet' -Name 'EnableActiveProbing' -Value 1
 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\NlaSvc\Parameters\Internet' -Name 'WebTimeout' -Value 23
 # Get Public IP @
-Set-Item -Path 'ENV:\IpAddressPublic' -Value '0.0.0.0'
-Set-Item -Path 'ENV:\IpAddressPublic' -Value (Invoke-WebRequest -Uri 'https://api.ipify.org' -UseBasicParsing).content
+Try { Set-Item -Path 'ENV:\IpAddressPublic' -Value (Invoke-WebRequest -Uri 'https://api.ipify.org' -UseBasicParsing).content }
+    Catch { Set-Item -Path 'ENV:\IpAddressPublic' -Value '0.0.0.0' }
 # Restart NLA Service for DomainController in order to gain extra Reboot in initial Deployment of ADS
 Get-CimInstance -NameSpace root/CIMV2 -ClassName win32_ComputerSystem | ForEach-Object { if ( $PSItem.DomainRole -in ( 4 , 5 ) ) { Restart-Service -Name 'NlaSvc' -Force } }
 # Get NLA state
@@ -25,5 +25,8 @@ Set-Item -Path 'ENV:\IPv4Connectivity' -Value (Get-NetConnectionProfile).IPv4Con
 $HotFixList = Get-HotFix | Select-Object -Property HotFixID,InstalledOn | Sort-Object -Property InstalledOn
 $LastHotFix = "$(($HotFixList | Where-Object { $PSItem.InstalledOn -eq $HotFixList[-1].InstalledOn }).HotFixID -Join ',') $($HotFixList[-1].InstalledOn.tostring('dd-MM-yyyy'))"
 Set-Item -Path 'ENV:\LastHotFix' -value $LastHotFix
+# Get WSUS Server
+Try { Set-Item -Path 'ENV:\WSUS' -Value ( Get-ItemPropertyValue -Path 'HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate' -Name 'WUServer' ) }
+    Catch { Set-Item -Path 'ENV:\WSUS' -Value '0.0.0.0' }
 # Launch BgInfo
 Invoke-Expression -Command "& .\bginfo.exe windows.bgi /timer:0 /nolicprompt"
