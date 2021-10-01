fcec736097753045a957c062756ccca2ab048849
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2021-10-01 15:14:42 +0200

    Update InstallVmWareTools.ps1
    Fix VmWareTools to version 11.3.5
    VmWare Product Interoperability Matrix
    Compatible Up To ESXi 7.0 U2

--- iNSTALL/Deployment/InstallVmWareTools.ps1
+++ iNSTALL/Deployment/InstallVmWareTools.ps1
@@ -3,8 +3,8 @@
 $SavedSecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol
 [System.Net.ServicePointManager]::SecurityProtocol = 'Tls12'
 Invoke-Expression ( New-Object System.Net.WebClient ).DownloadString( 'https://chocolatey.org/install.ps1' )
-# Restore Saved SecurityProtocol
-[System.Net.ServicePointManager]::SecurityProtocol = $SavedSecurityProtocol
 # Install vmware-tools version 11.3.5.18557794
 Invoke-Expression -Command '& C:\ProgramData\chocolatey\choco install vmware-tools --version=11.3.5.18557794 -y -f'
+# Restore Saved SecurityProtocol
+[System.Net.ServicePointManager]::SecurityProtocol = $SavedSecurityProtocol
 If ( $LASTEXITCODE -eq 3010 ) { Restart-Computer }
