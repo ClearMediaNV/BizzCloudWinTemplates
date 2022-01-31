997f39ab8b102d7c92a7213ee0eac7bf4ec70356
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2022-01-31 20:15:02 +0100

    Update WindowsUpdate.ps1
    Assure WindowsUpdateService Disabled By Default

--- iNSTALL/Deployment/WindowsUpdate.ps1
+++ iNSTALL/Deployment/WindowsUpdate.ps1
@@ -17,12 +17,14 @@ Try	{ $WindowsUpdateList = $WindowsUpdateSearch.Search($Null).Updates }
 	Catch 
 		{
         	Write-Output 'WSUS not Reachable. No Internet Connection. Please Check DNS & Gateway Config.'
+		Stop-Service -Name 'wuauserv' ; Set-Service -Name 'wuauserv' -StartupType 'Disabled'
         	Start-Sleep -Seconds 5
 		Exit
 		}
 If	( $WindowsUpdateList.Count -eq 0 )
 		{
 		Write-Output 'No Updates Available.'
+		Stop-Service -Name 'wuauserv' ; Set-Service -Name 'wuauserv' -StartupType 'Disabled'
 		Start-Sleep -Seconds 5
     		Exit
 		}
