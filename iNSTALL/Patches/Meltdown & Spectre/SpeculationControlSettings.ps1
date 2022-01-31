21a9921a6a1833c8a11c964f9594cd428546b7f2
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2022-01-31 20:35:07 +0100

    Update SpeculationControlSettings.ps1
    Improve Readability

--- iNSTALL/Patches/Meltdown & Spectre/SpeculationControlSettings.ps1
+++ iNSTALL/Patches/Meltdown & Spectre/SpeculationControlSettings.ps1
@@ -1,8 +1,4 @@
 Install-Module SpeculationControl
-# Save the current execution policy so it can be reset
-$SaveExecutionPolicy = Get-ExecutionPolicy
 Set-ExecutionPolicy RemoteSigned -Scope Currentuser
 Import-Module SpeculationControl
 Get-SpeculationControlSettings
-# Reset the execution policy to the original state
-Set-ExecutionPolicy $SaveExecutionPolicy -Scope Currentuser
