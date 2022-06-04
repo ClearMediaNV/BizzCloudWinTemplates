5e4714b5c66a724f4ba9eb69704ae69f9bf29503
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2022-06-04 23:51:42 +0200

    Update PresetAndBgInfo.ps1
    Improve Readability

--- iNSTALL/BgInfo/PresetAndBgInfo.ps1
+++ iNSTALL/BgInfo/PresetAndBgInfo.ps1
@@ -5,7 +5,7 @@ Set-DisplayResolution -Width 1280 -Height 800 -Force
 # Copy Shortcut to current Desktop
 Copy-Item -Path '.\PresetBginfo.lnk' -Destination "$Env:USERPROFILE\Desktop" -Force
 # Set Registry after SysPrep Reset - Windows Annoyances
-If ( 'Z' -notin ( Get-PSDrive ).Name ) { Get-CimInstance -Namespace 'root\cimv2' -ClassName 'Win32_Volume' -Filter 'DriveType = 5' | Set-CimInstance -argument  @{DriveLetter='Z:'} }
+If ( 'Z' -notin ( Get-PSDrive ).Name ) { Get-CimInstance -Namespace 'root\cimv2' -ClassName 'Win32_Volume' -Filter 'DriveType = 5' | Set-CimInstance -argument  @{ DriveLetter = 'Z:' } }
 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\Disk' -Name 'TimeOutValue' -Value 600
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\ServerManager' -Name 'DoNotOpenServerManagerAtLogon' -Value 1
 # Get Public IP @
@@ -19,7 +19,7 @@ Set-Item -Path 'ENV:\NetworkCategory' -value ( Get-NetConnectionProfile ).Networ
 Set-Item -Path 'ENV:\IPv4Connectivity' -Value ( Get-NetConnectionProfile ).IPv4Connectivity
 # Get Last Installed HotFix
 $HotFixList = Get-HotFix | Select-Object -Property HotFixID,InstalledOn | Sort-Object -Property InstalledOn
-$LastHotFix = "$( ( $HotFixList | Where-Object { $PSItem.InstalledOn -eq $HotFixList[-1].InstalledOn } ).HotFixID -Join ',' ) $( $HotFixList[-1].InstalledOn.tostring('dd-MM-yyyy') )"
+$LastHotFix = "$( ( $HotFixList | Where-Object { $PSItem.InstalledOn -eq $HotFixList[-1].InstalledOn } ).HotFixID -Join ',' ) $( $HotFixList[-1].InstalledOn.tostring( 'dd-MM-yyyy' ) )"
 Set-Item -Path 'ENV:\LastHotFix' -value $LastHotFix
 # Get WSUS Server
 Try { Set-Item -Path 'ENV:\WSUS' -Value ( Get-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate' ).WUServer }
