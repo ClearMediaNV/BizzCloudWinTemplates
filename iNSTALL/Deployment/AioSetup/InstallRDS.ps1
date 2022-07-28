b04c8ecf2e22fff75c75f43c06bdd238a1cfda8e
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2022-07-28 21:05:11 +0200

    Update InstallRDS.ps1

--- iNSTALL/Deployment/AioSetup/InstallRDS.ps1
+++ iNSTALL/Deployment/AioSetup/InstallRDS.ps1
@@ -2,17 +2,17 @@
 #  Please DO NOT Deploy RDS on a Standalone Server 2019  #
 #           Please contact ClearMedia Support            #
 ##########################################################
-# Installation of RDS Session Host will restart the System
+# Installation of RDS Session Host will Restart the System
 # Set RDS related Services to Automatic Start
 Set-Service -Name 'WSearch' -StartupType 'Auto'
 Set-Service -Name 'SCardSvr' -StartupType 'Auto'
 Set-Service -Name 'Audiosrv' -StartupType 'Auto'
 Set-Service -Name 'Spooler' -StartupType 'Auto'
-# Install Essential RDS Features & Roles
+# Install Essential RDS Features and Roles
 Install-WindowsFeature -Name Windows-TIFF-IFilter
 Install-WindowsFeature -Name RDS-RD-Server
 Install-WindowsFeature -Name RDS-Licensing
-Install-WindowsFeature -Name RDS-Licensing-UI, RSAT-RDS-Licensing-Diagnosis-UI
-# Disable RDS Fair Share on Cpu,Disk,Network Aka Handbrake
+Install-WindowsFeature -Name ( RDS-Licensing-UI , RSAT-RDS-Licensing-Diagnosis-UI )
+# Disable RDS Fair Share on ( Cpu , Disk , Network ) Aka HandBrake
 Get-CimInstance -Namespace 'root/cimv2/TerminalServices' -ClassName 'Win32_TerminalServiceSetting' | Set-CimInstance -Argument  @{ EnableDFSS = 0 ; EnableDiskFSS = 0 ; EnableNetworkFSS = 0 }
 Restart-Computer
