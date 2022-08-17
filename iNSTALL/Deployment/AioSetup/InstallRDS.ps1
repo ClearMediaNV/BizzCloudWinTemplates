1d51e248af4c399702bcc99b19cecf9a7f893533
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2022-08-17 21:47:22 +0200

    Update InstallRDS.ps1
    Improve Readability

--- iNSTALL/Deployment/AioSetup/InstallRDS.ps1
+++ iNSTALL/Deployment/AioSetup/InstallRDS.ps1
@@ -9,10 +9,10 @@ Set-Service -Name 'SCardSvr' -StartupType 'Auto'
 Set-Service -Name 'Audiosrv' -StartupType 'Auto'
 Set-Service -Name 'Spooler' -StartupType 'Auto'
 # Install Essential RDS Features and Roles
-Install-WindowsFeature -Name Windows-TIFF-IFilter
-Install-WindowsFeature -Name RDS-RD-Server
-Install-WindowsFeature -Name RDS-Licensing
-Install-WindowsFeature -Name ( RDS-Licensing-UI , RSAT-RDS-Licensing-Diagnosis-UI )
+Install-WindowsFeature -Name 'Windows-TIFF-IFilter'
+Install-WindowsFeature -Name 'RDS-RD-Server'
+Install-WindowsFeature -Name 'RDS-Licensing'
+Install-WindowsFeature -Name ( 'RDS-Licensing-UI' , 'RSAT-RDS-Licensing-Diagnosis-UI' )
 # Disable RDS Fair Share on ( Cpu , Disk , Network ) Aka HandBrake
 Get-CimInstance -Namespace 'root/cimv2/TerminalServices' -ClassName 'Win32_TerminalServiceSetting' | Set-CimInstance -Argument  @{ EnableDFSS = 0 ; EnableDiskFSS = 0 ; EnableNetworkFSS = 0 }
 Restart-Computer
