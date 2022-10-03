fb00affcdc642ea3ea657914dd72d7ac56179bdb
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2022-10-03 16:06:19 +0200

    Update GetO365TenantID.ps1
    Roll Back

--- iNSTALL/O365/GetO365TenantID.ps1
+++ iNSTALL/O365/GetO365TenantID.ps1
@@ -12,7 +12,30 @@ $Form.StartPosition = 'CenterScreen'
 $Form.FormBorderStyle = 'fixedsingle'
 $Form.Topmost = $False
 
+# Add Action ScriptBlocks
+$ScriptBlockOK = {
+    try {
+        $Domainname = $TextBoxDomainName.Text
+        $Url = "https://login.windows.net/$Domainname/.well-known/openid-configuration"
+        $TextBoxTenantID.Text = ( Invoke-RestMethod -Uri $Url ).token_endpoint.Split('/')[3]
+        $TextBoxTenantID.BackColor = 'LightGreen'
+        $TextBoxTenantID.Refresh()
+        }
+    catch {
+        $TextBoxTenantID.Text = "$DomainName not found"
+        $TextBoxTenantID.BackColor = 'Red'
+        $TextBoxTenantID.Refresh()
+        }
+    }
+$ScriptBlockCspDelegation = {
+    $Url = 'https://bit.ly/3pWul40'
+    Start-Process $Url
+    }
+$ScriptBlockCopyToClipboardDomain = { Set-Clipboard -Value $TextBoxDomainName.Text }
+$ScriptBlockCopyToClipboardTenantID = { Set-Clipboard -Value $TextBoxTenantID.Text }
+
 # Add OK Button
+# On Click or Enter launch ScriptBlock
 $ButtonOK = New-Object System.Windows.Forms.Button
 $ButtonOK.Left = 40
 $ButtonOK.Top = 150
@@ -20,9 +43,9 @@ $ButtonOK.Width = 75
 $ButtonOK.Height = 40
 $ButtonOK.BackColor = 'White'
 $ButtonOK.Text = 'OK'
-$ButtonOK.Add_Click( $ScriptBlockOK )
+$ButtonOK.Add_Click($ScriptBlockOK)
 $Form.AcceptButton = $ButtonOK
-$Form.Controls.Add( $ButtonOK )
+$Form.Controls.Add($ButtonOK)
 
 # Add CspDelegation Button
 $ButtonCspDelegation = New-Object System.Windows.Forms.Button
@@ -32,10 +55,11 @@ $ButtonCspDelegation.Width = 290
 $ButtonCspDelegation.Height = 40
 $ButtonCspDelegation.BackColor = 'LightYellow'
 $ButtonCspDelegation.ForeColor = 'Red'
-$ButtonCspDelegation.Font = new-object System.Drawing.Font( '' , 8 , [System.Drawing.FontStyle]::Bold )
+$ButtonCspDelegation.Font = new-object System.Drawing.Font('',8,[System.Drawing.FontStyle]::Bold)
 $ButtonCspDelegation.Text = 'Start CSP Partner Relationship to ClearMedia NV'
-$ButtonCspDelegation.Add_Click( $ScriptBlockCspDelegation )
-$Form.Controls.Add( $ButtonCspDelegation )
+$ButtonCspDelegation.Add_Click($ScriptBlockCspDelegation)
+$Form.Controls.Add($ButtonCspDelegation)
+
 
 # Add Cancel Button
 $ButtonCancel = New-Object System.Windows.Forms.Button
@@ -46,7 +70,7 @@ $ButtonCancel.Height = 40
 $ButtonCancel.BackColor = 'White'
 $ButtonCancel.Text = 'CANCEL'
 $Form.CancelButton = $ButtonCancel
-$Form.Controls.Add( $ButtonCancel )
+$Form.Controls.Add($ButtonCancel)
 
 # Add CopyToClipboardDomain Button
 $ButtonCopyToClipboardDomain = New-Object System.Windows.Forms.Button
@@ -56,8 +80,8 @@ $ButtonCopyToClipboardDomain.Width = 50
 $ButtonCopyToClipboardDomain.Height = 30
 $ButtonCopyToClipboardDomain.BackColor = 'White'
 $ButtonCopyToClipboardDomain.Text = 'Copy'
-$ButtonCopyToClipboardDomain.Add_Click( $ScriptBlockCopyToClipboardDomain )
-$Form.Controls.Add( $ButtonCopyToClipboardDomain )
+$ButtonCopyToClipboardDomain.Add_Click($ScriptBlockCopyToClipboardDomain)
+$Form.Controls.Add($ButtonCopyToClipboardDomain)
 
 # Add CopyToClipboardTenantID Button
 $ButtonCopyToClipboardTenantID = New-Object System.Windows.Forms.Button
@@ -67,8 +91,8 @@ $ButtonCopyToClipboardTenantID.Width = 50
 $ButtonCopyToClipboardTenantID.Height = 30
 $ButtonCopyToClipboardTenantID.BackColor = 'White'
 $ButtonCopyToClipboardTenantID.Text = 'Copy'
-$ButtonCopyToClipboardTenantID.Add_Click( $ScriptBlockCopyToClipboardTenantID )
-$Form.Controls.Add( $ButtonCopyToClipboardTenantID )
+$ButtonCopyToClipboardTenantID.Add_Click($ScriptBlockCopyToClipboardTenantID)
+$Form.Controls.Add($ButtonCopyToClipboardTenantID)
 
 # Add DomaiName Label
 $LabelDomainName = New-Object System.Windows.Forms.Label
@@ -79,7 +103,7 @@ $LabelDomainName.Width = 400
 $LabelDomainName.Height = 30
 $LabelDomainName.Font = 'Candara , 11pt, style=Regular'
 $LabelDomainName.Text = 'Please enter the Domain Name'
-$Form.Controls.Add( $LabelDomainName )
+$Form.Controls.Add($LabelDomainName)
 
 # Add DomainName TextBox
 $TextBoxDomainName = New-Object System.Windows.Forms.TextBox
@@ -92,7 +116,7 @@ $TextBoxDomainName.BackColor = 'green'
 $TextBoxDomainName.Font = 'Candara , 14pt, style=Bold'
 $TextBoxDomainName.TextAlign = 'Center'
 $TextBoxDomainName.Text = 'ClearMedia.be'
-$Form.Controls.Add( $TextBoxDomainName )
+$Form.Controls.Add($TextBoxDomainName)
 
 # Add TenantID TextBox
 $TextBoxTenantID = New-Object System.Windows.Forms.TextBox
@@ -105,27 +129,6 @@ $TextBoxTenantID.Height = 30
 $TextBoxTenantID.Font = 'Candara , 14pt, style=Bold'
 $TextBoxTenantID.TextAlign = 'Center'
 $TextBoxTenantID.Text = ''
-$Form.Controls.Add( $TextBoxTenantID )
+$Form.Controls.Add($TextBoxTenantID)
 
-# Add Action ScriptBlocks
-$ScriptBlockOK = {
-    Try {
-        $Domainname = $TextBoxDomainName.Text
-        [System.Net.ServicePointManager]::SecurityProtocol = 'Tls12'
-        $Url = "https://login.windows.net/$Domainname/.well-known/openid-configuration"
-        $TextBoxTenantID.Text = ( Invoke-RestMethod -Uri $Url ).token_endpoint.Split('/')[3]
-        $TextBoxTenantID.BackColor = 'LightGreen'
-        $TextBoxTenantID.Refresh()
-        }
-    Catch {
-        $TextBoxTenantID.Text = "$DomainName not found"
-        $TextBoxTenantID.BackColor = 'Red'
-        $TextBoxTenantID.Refresh()
-        }
-    }
-$ScriptBlockCspDelegation = { Start-Process 'https://bit.ly/3pWul40' }
-$ScriptBlockCopyToClipboardDomain = { Set-Clipboard -Value $TextBoxDomainName.Text }
-$ScriptBlockCopyToClipboardTenantID = { Set-Clipboard -Value $TextBoxTenantID.Text }
-
-# Show Form Aka Start Show
 $Form.ShowDialog()
