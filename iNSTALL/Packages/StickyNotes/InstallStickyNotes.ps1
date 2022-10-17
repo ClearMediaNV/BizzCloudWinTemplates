33c8dc7f51a79e7292c545894af12488ad727f12
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2022-10-17 10:53:08 +0200

    Delete iNSTALL/Packages/StickyNotes directory

--- iNSTALL/Packages/StickyNotes/InstallStickyNotes.ps1
+++
@@ -1,5 +0,0 @@
-# DownloadInstall Chocolatey as PackageManager
-[System.Net.ServicePointManager]::SecurityProtocol = 'Tls12'
-Invoke-Expression ( New-Object System.Net.WebClient ).DownloadString( 'https://chocolatey.org/install.ps1' )
-# Install StickyNotes
-Invoke-Expression -Command '& C:\ProgramData\chocolatey\choco install simple-sticky-notes -y -f'
