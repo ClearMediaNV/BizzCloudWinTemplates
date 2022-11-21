911767c75552e382a22173dc34e742424abdfc59
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2022-11-21 10:26:53 +0100

    Update SyncGit.ps1
    Improve Readability

--- iNSTALL/SyncGit.ps1
+++ iNSTALL/SyncGit.ps1
@@ -1,25 +1,21 @@
-# Get GitHub Repository Archive
-# Change SecurityProtocol for downloading from GitHub
-[System.Net.ServicePointManager]::SecurityProtocol = 'Tls12'
 Try {
+    # Get GitHub Repository Archive
+    # Change SecurityProtocol for downloading from GitHub
+    [System.Net.ServicePointManager]::SecurityProtocol = 'Tls12'
     $Branch = 'master'
     $UrlDownload = "https://github.com/ClearMediaNV/BizzCloudWinTemplates/archive/$Branch.zip"
     $FileDownload = "$ENV:LOCALAPPDATA\$Branch.zip"
     $FolderDownload = "$ENV:LOCALAPPDATA\$Branch"
-    # Download Archive
-    ( New-Object System.Net.WebClient ).DownLoadFile( $UrlDownload , $FileDownload )
-    # Unzip Archive to Folder Download
-    [VOID][System.Reflection.Assembly]::LoadWithPartialName( "System.IO.Compression.FileSystem" )
-    [System.IO.Compression.ZipFile]::ExtractToDirectory( $FileDownload , $FolderDownload ) 
-    # Cleanup and Copy iNSTALL Folder
-    Remove-Item -Path 'c:\install\*'  -Recurse -Force
-    Copy-Item -Path "$FolderDownload\BizzCloudWinTemplates-$Branch\iNSTALL\*" -Destination 'c:\iNSTALL' -Recurse -Force
-    # Cleanup File Download and Folder Download
+    # Download UrlDownload to FileDownload
+    (New-Object System.Net.WebClient).downloadFile($UrlDownload,$FileDownload)
+    # Unzip FileDownload to FolderDownload
+    Expand-Archive -Path $FileDownload -DestinationPath $FolderDownload -Force
+    # Cleanup and Copy Install Folder
+    Remove-Item -Path 'C:\Install\*' -Recurse -Force
+    Copy-Item -Path "$FolderDownload\BizzCloudWinTemplates-$Branch\Install\*" -Destination 'C:\Install' -Recurse -Force
+    # Cleanup FileDownload and FolderDownload
     Remove-Item -Path "$FileDownload" -Force
     Remove-Item -Path "$FolderDownload" -Recurse -Force
     }
-   Catch {
-         Write-Output 'GitHub Connection Error. Please Check DNS & Gateway Config. Please Check https://github.com/ClearMediaNV'
-         Start-Sleep -Seconds 5
-         }
+   Catch { Write-Output 'GitHub Connection Error. Please Check DNS & Gateway Config. Please Check https://github.com/ClearMediaNV' }
 # The End
