95728ce69caab7414e3ade6adf29961e41dc0154
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2023-12-18 18:18:19 +0100

    Update InstallVmWareTools.ps1
    Install vmware-tools version 12.3.5.22544099

--- iNSTALL/Deployment/InstallVmWareTools.ps1
+++ iNSTALL/Deployment/InstallVmWareTools.ps1
@@ -2,6 +2,6 @@
 # Change SecurityProtocol for downloading from Chocolatey
 [System.Net.ServicePointManager]::SecurityProtocol = 'Tls12'
 Invoke-Expression ( New-Object System.Net.WebClient ).DownloadString( 'https://chocolatey.org/install.ps1' )
-# Install vmware-tools version 12.3.0.22234872
-Invoke-Expression -Command '& C:\ProgramData\chocolatey\choco install vmware-tools --version=12.3.0.22234872 -y -f'
+# Install vmware-tools version 12.3.5.22544099
+Invoke-Expression -Command '& C:\ProgramData\chocolatey\choco install vmware-tools --version=12.3.5.22544099 -y -f'
 If ( $LASTEXITCODE -eq 3010 ) { Restart-Computer }
