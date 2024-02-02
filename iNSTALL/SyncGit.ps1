c5903a94280289d31c37ce00c082d15392cea5dc
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2024-02-02 11:25:59 +0100

    Update SyncGit.ps1
    Improve Readability

--- iNSTALL/SyncGit.ps1
+++ iNSTALL/SyncGit.ps1
@@ -7,7 +7,7 @@ Try {
     $FileDownload = "$ENV:LOCALAPPDATA\$Branch.zip"
     $FolderDownload = "$ENV:LOCALAPPDATA\$Branch"
     # Download UrlDownload to FileDownload
-    ( New-Object System.Net.WebClient ).DownloadFile( $UrlDownload , $FileDownload )
+    Invoke-WebRequest -Uri $UrlDownload -OutFile $FileDownload -UseBasicParsing
     # Unzip FileDownload to FolderDownload
     Expand-Archive -Path $FileDownload -DestinationPath $FolderDownload -Force
     # Cleanup and Copy Install Folder
