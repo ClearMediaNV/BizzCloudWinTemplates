19c7c17124af7b7b109ea5f59fbe922d1ca8a17a
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2024-05-15 15:24:46 +0200

    Update InstallMicrosoftEdge.ps1
    Install Microsoft Edge for business version 124.0.2478.105

--- iNSTALL/Deployment/InstallMicrosoftEdge.ps1
+++ iNSTALL/Deployment/InstallMicrosoftEdge.ps1
@@ -2,7 +2,7 @@
 # Change SecurityProtocol for downloading from Chocolatey
 [System.Net.ServicePointManager]::SecurityProtocol = 'Tls12'
 Invoke-Expression ( New-Object System.Net.WebClient ).DownloadString( 'https://chocolatey.org/install.ps1' )
-# Install Microsoft Edge for business version 124.0.2478.97
+# Install Microsoft Edge for business version 124.0.2478.105
 # Microsoft Edge for business @ https://www.microsoft.com/en-us/edge/business/download
-# Microsoft Edge for business version 124.0.2478.97 GPO Templates @ https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/587098a7-2662-4a9d-94ab-4704c588c09e/MicrosoftEdgePolicyTemplates.cab
-Invoke-Expression -Command '& C:\ProgramData\chocolatey\choco install microsoft-edge --version=124.0.2478.97 -y -f'
+# Microsoft Edge for business version 124.0.2478.97 GPO Templates @ https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/75eeb64a-9021-4366-b602-840c2d3b343e/MicrosoftEdgePolicyTemplates.cab
+Invoke-Expression -Command '& C:\ProgramData\chocolatey\choco install microsoft-edge --version=124.0.2478.105 -y -f'
