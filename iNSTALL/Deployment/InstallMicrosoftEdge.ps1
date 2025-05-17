8642da808329621cfdcfdd2e1466562bb116a5ff
guyjochmans <33223238+guyjochmans@users.noreply.github.com>
2025-05-17 22:17:42 +0200

    Update InstallMicrosoftEdge.ps1
    Deploy 'Microsoft Edge for business' version 136.0.3240.76

--- iNSTALL/Deployment/InstallMicrosoftEdge.ps1
+++ iNSTALL/Deployment/InstallMicrosoftEdge.ps1
@@ -4,5 +4,5 @@
 Invoke-Expression ( New-Object System.Net.WebClient ).DownloadString( 'https://chocolatey.org/install.ps1' )
 # Install Microsoft Edge for business version 136.0.3240.76
 # Microsoft Edge for business @ https://www.microsoft.com/en-us/edge/business/download
-# Microsoft Edge for business version 134.0.3124.85 GPO Templates @ https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/c787a8dd-98f2-4f8f-9947-28db1206295d/MicrosoftEdgePolicyTemplates.cab
+# Microsoft Edge for business version 136.0.3240.76 GPO Templates @ https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/c787a8dd-98f2-4f8f-9947-28db1206295d/MicrosoftEdgePolicyTemplates.cab
 Invoke-Expression -Command '& C:\ProgramData\chocolatey\choco install microsoft-edge --version=136.0.3240.76 -y -f'
